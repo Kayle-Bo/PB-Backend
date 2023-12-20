087f01fcb5e17870bb6c48d6b46ea88bf56e838b
Kayle (School) <i493302@fhict.nl>
2023-12-20 19:35:07 +0100

    Added orm for login

--- index.js
+++ index.js
@@ -51,91 +51,97 @@ server.listen(3001, () => {
     console.log('listening on *:3001');
 });
 
-// On connection
-io.on('connection', (socket) => {
+dataSource.initialize().then(function(){
 
-    console.log(`User connected: ${socket.id}`);
-    let users = new Map(io.sockets.adapter.sids);
-    let rooms = new Map(io.sockets.adapter.rooms);
-    let nonMatches = new Map([...users].filter(([key]) => !rooms.has(key)));
-    actualRooms = new Map([...nonMatches, ...rooms].filter(([key]) => !users.has(key)));
-    console.log("nonMatches")
-    console.log(nonMatches)
-    //let actualRooms = getNonMatches(users, rooms)
-    //console.log(actualRooms)
+    // On connection
+    io.on('connection', (socket) => {
 
-    socket.on("register_user", (user, cb) =>{
-        dataSource.initialize().then(function(){
+        console.log(`User connected: ${socket.id}`);
+        let users = new Map(io.sockets.adapter.sids);
+        let rooms = new Map(io.sockets.adapter.rooms);
+        let nonMatches = new Map([...users].filter(([key]) => !rooms.has(key)));
+        actualRooms = new Map([...nonMatches, ...rooms].filter(([key]) => !users.has(key)));
+        console.log("nonMatches")
+        console.log(nonMatches)
+        //let actualRooms = getNonMatches(users, rooms)
+        //console.log(actualRooms)
+
+        socket.on("register_user", (user, cb) =>{
             var userRepository = dataSource.getRepository("user");
-
-
             userRepository.save(user);
-            console.log("user saved")
+
+        });
+
+        socket.on("login_user", (user, cb) => {
             console.log(user)
-        })
-
-    });
-
-    socket.on("login_user", (user, cb) => {
-        db.query(`SELECT username FROM users WHERE username = '${user.username}' AND password = '${user.password}'`, (err, result) => {
-            try {
-                if(result != null){
-                    cb(result[0].username);
+            var userRepository = dataSource.getRepository("user");
+            userRepository.findOneBy({username: user.username}).then(function(user){
+                if(user != null){
+                    userRepository.findOneBy({username: user.username, password: user.password}).then(function(user){
+                        if(user != null){
+                            userDTO = {
+                                id: user.id,
+                                username: user.username,
+                                email: user.email,
+                                wins: user.wins,
+                                losses: user.losses
+                            }
+                            cb(userDTO);
+                        }
+                        else{
+                            cb('0');
+                        }
+                    })
                 }
                 else{
                     cb('0');
                 }
-            } catch (err) {
-                console.log(err);
-                // If there was an error, return 0
-                cb('0');
-            }
+            })
         });
-    });
 
 
 
 
-    socket.on("create_battle", () => {
-        let battleId = generateRandomString();
-        socket.emit("battle_created", battleId);
+        socket.on("create_battle", () => {
+            let battleId = generateRandomString();
+            socket.emit("battle_created", battleId);
 
-    });
+        });
 
-    socket.on("get_rooms", () => {
-        console.log("get_rooms")
-        console.log(new Map(io.sockets.adapter.rooms));
-    });
+        socket.on("get_rooms", () => {
+            console.log("get_rooms")
+            console.log(new Map(io.sockets.adapter.rooms));
+        });
 
-    socket.on("join_room", ({ roomId, userId }) => {
-        console.log(`User with ID: ${userId} joined room: ${roomId}`);
+        socket.on("join_room", ({ roomId, userId }) => {
+            console.log(`User with ID: ${userId} joined room: ${roomId}`);
 
-        // Emit that the room has been created with the id of the room
-        io.emit("room_created", roomId);
-        // Emit that the user has joined the room with the id of the user
-        io.emit("user_joined", userId);
-    });
+            // Emit that the room has been created with the id of the room
+            io.emit("room_created", roomId);
+            // Emit that the user has joined the room with the id of the user
+            io.emit("user_joined", userId);
+        });
 
-    socket.on("old_user", (userId) => {
-        io.emit("existing_user", userId);
-    });
+        socket.on("old_user", (userId) => {
+            io.emit("existing_user", userId);
+        });
 
-    socket.on("room_filled", (room) => {
-        io.emit("room_remove", room);
-    });
+        socket.on("room_filled", (room) => {
+            io.emit("room_remove", room);
+        });
 
-    socket.on("disconenct", () => {
-        console.log(`User disconnected: ${socket.id}`);
+        socket.on("disconenct", () => {
+            console.log(`User disconnected: ${socket.id}`);
 
-        let message = `User: ${socket.id} left`;
-        socket.to(data).emit("receive_message", message);
-    });
+            let message = `User: ${socket.id} left`;
+            socket.to(data).emit("receive_message", message);
+        });
 
-    socket.on('send_message', (data) => {
-        let message = `${data.userId}: ${data.message}`;
-        socket.to(data.room).emit('receive_message', message);
-    });
+        socket.on('send_message', (data) => {
+            let message = `${data.userId}: ${data.message}`;
+            socket.to(data.room).emit('receive_message', message);
+        });
 
+    })
 })
-
 
